9cd0267cd1a1f5bc389155dcfdf86129e17a4bed
Sean Jun <seanjun21@gmail.com>
2016-09-19 11:08:38 -0700

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+const packageData = require('./package.json');
+
+const filename = [packageData.name, packageData.version, 'js'];
+
+module.exports = {
+    entry: path.resolve(__dirname, js/index.jsx),
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: filename.join('.')
+    },
+    devtool: 'source-map',
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /(node_modules)/,
+            loader: 'babel',
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    }
+};
