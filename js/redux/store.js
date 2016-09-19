2abbddbad315d266b2b7381a6b8bad7ea90966d3
Alex Bang (MBP) <alexbang@hotmail.com>
2016-09-19 19:42:49 -0400

    Adds store

---
+++ js/redux/store.js
@@ -0,0 +1,10 @@
+import {createStore, applyMiddleware, compose} from 'redux';
+import reducer from './reducers';
+const thunk = require('redux-thunk').default;
+
+const store = createStore(reducer, compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+export default store;
