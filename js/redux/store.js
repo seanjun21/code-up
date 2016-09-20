03513b1f414d05533cfd4b7332617002a43af33d
Alex Bang (MBP) <alexbang@hotmail.com>
2016-09-20 15:08:02 -0400

    changes name reducers

--- js/redux/store.js
+++ js/redux/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, compose} from 'redux';
-import reducer from './reducers';
+import reducer from './reducer';
 import createSocketIoMiddleware from 'redux-socket.io';
 import io from 'socket.io-client';
 
