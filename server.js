d962493832c0eb50013881a723f5fa8383fe0c23
Joe Seago <joe.seago@gmail.com>
2016-09-20 07:59:24 -0600

    attempt db connection

--- server.js
+++ server.js
@@ -8,6 +8,19 @@ const addUser = require('./backend/functions/add-user');
 const postMessage = require('./backend/functions/post-message');
 const postQuestion = require('./backend/functions/post-question');
 const filterQuestions = require('./backend/functions/filter-questions');
+const pg = require('pg');
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
 
 const sockets = [];
 
