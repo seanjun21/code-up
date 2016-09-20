ff61e096bfbee937cdedf7d89ad8ce8fd553304f
Alex Bang (MBP) <alexbang@hotmail.com>
2016-09-20 15:11:23 -0400

    Updates store

--- js/redux/store.js
+++ js/redux/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, compose} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import reducer from './reducer';
 import createSocketIoMiddleware from 'redux-socket.io';
 import io from 'socket.io-client';
