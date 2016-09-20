bbe0d2fa26f5e78c045dbde7eaefdb9fc8347ea8
Alex Bang (MBP) <alexbang@hotmail.com>
2016-09-20 18:55:21 -0400

    Change routes to include landing, login, questions, and chatroom page

--- js/components/landing-page.jsx
+++ js/components/landing-page.jsx
@@ -11,18 +11,25 @@ class LandingPage extends React.Component{
       <div className="container">
         <div className="app-name">
           <h1>Code Roulette</h1>
-          <h2>Sign in to continue</h2>
+          <h2>Log in</h2>
         </div>
         <div>
           <UserName />
         </div>
         <div className="question-feed">
+          <p>Log in to submit or answer questions</p>
           <ul>
+            <li>question 1</li>
+            <li>question 2</li>
+            <li>question 3</li>
+            <li>question 4</li>
+            <li>question 5</li>
           </ul>
         </div>
-        <div className="post-question">
+        {/* <div className="post-question">
+          <p>Submit a question:</p>
           <input className="post-question-input"></input>
-        </div>
+        </div> */}
       </div>
     );
   }
