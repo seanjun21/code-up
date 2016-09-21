ace228695b4479e2788754e5a09daf41dfb05fbf
Joe Seago <joe.seago@gmail.com>
2016-09-21 12:41:29 -0600

    changed connect.js knex syntax

--- backend/database/connect.js
+++ backend/database/connect.js
@@ -2,12 +2,14 @@ const pg = require('pg');
 const knex = require('knex')(pg);
 pg.defaults.ssl = true;
 pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-  knex.query('SELECT * FROM users', function(err, result) {
-    done();
-    if(err) return console.error(err);
+  knex.select().from('users').then(function(result) {
+    return result
     console.log(result.rows);
-  });
-});
+  })
+  .catch(function(err) {
+  	console.error(err);
+  })
+})
 
 module.exports = pg;
 
