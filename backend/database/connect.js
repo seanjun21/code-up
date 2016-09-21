e84c45518b6d2fdde08c140ef949b3a4da2ec944
Joe Seago <joe.seago@gmail.com>
2016-09-21 12:36:25 -0600

    tweaked connect.js database variable

--- backend/database/connect.js
+++ backend/database/connect.js
@@ -1,7 +1,7 @@
 const pg = require('pg');
 const knex = require('knex')(pg);
 pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL?ssl=true, function(err, client, done) {
+pg.connect(process.env.DATABASE_URL, function(err, client, done) {
   knex.query('SELECT * FROM users', function(err, result) {
     done();
     if(err) return console.error(err);
