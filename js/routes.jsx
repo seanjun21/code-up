6689169a2a82c56a3522e589476684db342b0d7f
Alex Bang (MBP) <alexbang@hotmail.com>
2016-09-24 20:31:14 -0400

    Adds NavBar component

--- js/routes.jsx
+++ js/routes.jsx
@@ -2,11 +2,20 @@ import React from 'react';
 import {Router, Route, IndexRoute, hashHistory} from 'react-router';
 import LandingPage from './components/landing-page';
 import ChatroomPage from './components/chatroom-page';
+import NavBar from './components/navBar';
 
 let App = (props) => {
   return (
     <div className='app'>
-      <div>
+      {/* <div> */}
+        <div className="container">
+          <div className="navigation">
+            <h1 id="app-name">Code Roulette</h1>
+            <h4 id="beta">beta</h4>
+              <div className="userName">
+              <UserName userName={userName} />
+              </div>
+          </div>
         {props.children}
       </div>
     </div>
