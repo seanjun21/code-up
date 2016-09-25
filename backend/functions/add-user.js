361864452a84a16a7992eb0f87cdc06c518343c3
Sean Jun <seanjun21@gmail.com>
2016-09-25 11:57:22 -0700

    add-user.js and server.js updated

--- backend/functions/add-user.js
+++ backend/functions/add-user.js
@@ -4,14 +4,16 @@ let addUser = (data) => {
     return new Promise((resolve, reject) => {
         let userName = data.input;
         knex.insert({ user_name: userName })
-        .returning('id', 'user_name')
-        .into('users')
-        .then((id, user_name) => {
-            resolve({ userID: id, userName: user_name });
-        })
-        .catch((err) => {
-            reject(err);
-        });
+            .returning(['id', 'user_name'])
+            .into('users')
+            .then((data) => {
+                resolve({
+                    userID: data[0].id, userName: data[0].user_name
+                });
+            })
+            .catch((err) => {
+                reject(err);
+            });
     });
 };
 
--- server.js
+++ server.js
@@ -14,16 +14,16 @@ const filterQuestions = require('./backend/functions/filter-questions');
 // const joinRoom = require('./backend/functions/join-room');
 
 let spaces = {
-                lobby: []
-              };
+    lobby: []
+};
 
 app.use(express.static('./build'));
 
 io.on('connection', (socket) => {
 
-    console.log("socket------->")
-    console.log(socket)
-    console.log(`Socket connected: ${socket.id}`);
+    // console.log("socket------->");
+    // console.log(socket);
+    // console.log(`Socket connected: ${socket.id}`);
 
     spaces.lobby.push(socket);
     socket.on('action', (action) => {
@@ -39,45 +39,61 @@ io.on('connection', (socket) => {
         if (action.type === 'server/addUser') {
             // TODO: send back username to be stored in state in 'lobby' array. emit to everyone for update
             addUser(action.data).then((data) => {
+                let userNameArr = [];
+                spaces.lobby.forEach((person) => {
+                    if (socket.id === person.id) {
+                        person.userName = data.userName;
+                        person.userID = data.userID;
+                    }
+                    if (person.userName) {
+                        let personObj = {
+                            userName: person.userName,
+                            userID: person.userID
+                        };
+                        userNameArr.push(personObj);
+                    }
+                });
+                console.log(userNameArr, '<<<<<<<userNameArr');
+
                 spaces.lobby.forEach((socket) => {
                     socket.emit('action', {
                         type: 'userEnterLobby',
-                        data: data
+                        data: userNameArr
                     });
                 });
             });
         }
         if (action.type === 'server/postMessage') {
-            let questionID = action.data.questionID
+            let questionID = action.data.questionID;
             postMessage(action.data).then((data) => {
                 spaces[questionID].forEach((socket) => {
-                   socket.emit('action', {
-                       type: 'postMessageSuccess',
-                       data: data
-                   });
+                    socket.emit('action', {
+                        type: 'postMessageSuccess',
+                        data: data
+                    });
                 });
             });
         }
         if (action.type === 'server/postQuestion') {
-            let questionID = action.data.questionID
-            spaces[questionID] = [socket]
+            let questionID = action.data.questionID;
+            spaces[questionID] = [socket];
 
-            lobby = spaces[lobby]
+            let lobby = spaces[lobby];
             for (let j = 0; j < lobby.length; j += 1) {
-              let roomSock = lobby[j];
-              console.log('Room Socket ----> ', roomSock.id);
-              console.log('Socket to delete ----> ', socket.id);
-              if (lobbySock.id === socket.id) {
-                lobby.splice(j, 1);
-              }
+                let roomSock = lobby[j];
+                // console.log('Room Socket ----> ', roomSock.id);
+                // console.log('Socket to delete ----> ', socket.id);
+                if (lobbySock.id === socket.id) {
+                    lobby.splice(j, 1);
+                }
             }
 
             postQuestion(action.data).then((data) => {
                 spaces.lobby.forEach((socket) => {
-                   socket.emit('action', {
-                       type: 'postQuestionSuccess',
-                       data: data
-                   });
+                    socket.emit('action', {
+                        type: 'postQuestionSuccess',
+                        data: data
+                    });
                 });
             });
         }
@@ -91,55 +107,55 @@ io.on('connection', (socket) => {
         }
         // TODO: pass back the username for that room and store room's occupants (add section that shows room's occuments in chatroom component and make sure we send back the user that joined the room to be stored as state also.)
         if (action.type === 'server/joinRoom') {
-          let questionID = action.data.questionID
-          spaces[questionID].push(socket)
+            let questionID = action.data.questionID;
+            spaces[questionID].push(socket);
 
-          lobby = spaces[lobby]
-          for (let j = 0; j < lobby.length; j += 1) {
-            let roomSock = lobby[j];
-            console.log('Room Socket ----> ', roomSock.id);
-            console.log('Socket to delete ----> ', socket.id);
-            if (lobbySock.id === socket.id) {
-              lobby.splice(j, 1);
+            let lobby = spaces[lobby];
+            for (let j = 0; j < lobby.length; j += 1) {
+                let roomSock = lobby[j];
+                // console.log('Room Socket ----> ', roomSock.id);
+                // console.log('Socket to delete ----> ', socket.id);
+                if (lobbySock.id === socket.id) {
+                    lobby.splice(j, 1);
+                }
             }
-          }
 
-          joinRoom(action.data).then((data) => {
-              socket[questionID].forEach((socket) => {
-                  socket.emit('action', {
-                    type: 'joinRoomSuccess',
-                    data: data
-                  });
-              });
-          });
+            joinRoom(action.data).then((data) => {
+                socket[questionID].forEach((socket) => {
+                    socket.emit('action', {
+                        type: 'joinRoomSuccess',
+                        data: data
+                    });
+                });
+            });
         }
     });
 //when a user leaves or closes browser
-socket.on('disconnect', () => {
-  var rooms = Object.keys(spaces);
-  var socketToRemove = socket.id;
+    socket.on('disconnect', () => {
+        var rooms = Object.keys(spaces);
+        var socketToRemove = socket.id;
 
-  for (let i = 0; i < rooms.length; i += 1) {
-    let room = spaces[rooms[i]];
-    console.log(`room ${rooms[i]} ----> `, room )
-    for (let j = 0; j < room.length; j += 1) {
-      let roomSock = room[j];
-      console.log('Room Socket ----> ', roomSock.id);
-      console.log('Socket to delete ----> ', socket.id);
-      if (roomSock.id === socket.id) {
-        room.splice(j, 1);
-      }
-    }
-    console.log(`${rooms[i]} ----> `, room )
-  }
+        for (let i = 0; i < rooms.length; i += 1) {
+            let room = spaces[rooms[i]];
+            // console.log(`room ${rooms[i]} ----> `, room );
+            for (let j = 0; j < room.length; j += 1) {
+                let roomSock = room[j];
+                // console.log('Room Socket ----> ', roomSock.id);
+                // console.log('Socket to delete ----> ', socket.id);
+                if (roomSock.id === socket.id) {
+                    room.splice(j, 1);
+                }
+            }
+            // console.log(`${rooms[i]} ----> `, room )
+        }
 
-  });
-})
+    });
+});
 
 function runServer(callback) {
     let PORT = process.env.PORT || 8080;
     server.listen(PORT, () => {
-        console.log(`Listening on localhost: ${PORT}`);
+        // console.log(`Listening on localhost: ${PORT}`);
         if (callback) {
             callback();
         }
@@ -152,4 +168,4 @@ if (require.main === module) {
             throw new Error(err);
         }
     });
-};
+}
