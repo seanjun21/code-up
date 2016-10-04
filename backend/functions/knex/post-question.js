1c6b2db798012b7a51a3f44cfb1f014910dc395d
Sean Jun <seanjun21@gmail.com>
2016-10-04 16:56:32 -0700

    tags are being added to database

--- backend/functions/knex/post-question.js
+++ backend/functions/knex/post-question.js
@@ -5,59 +5,79 @@ let postQuestion = (data) => {
     let userID = data.userID;
     let tags = data.tags;
     return new Promise((resolve, reject) => {
-        let currentQuestion = insertQ(questionText, userID);
-        let insertedTags = insertT(tags, currentQuestion.id);
-        knex.insert()
-        .from('questions')
-        .where({ is_answered: false })
-        .orderBy('when_asked')
-        .then((questions) => {
-            resolve({
-                questions: questions,
-                currentQuestion: {
-                    questionID: currentQuestion.id,
-                    questionText: currentQuestion.question_text,
-                    whenAsked: currentQuestion.when_asked,
-                    messages: [],
-                    insertedTags: insertedTags
-                }
+        const promiseQ = insertQ(questionText, userID);
+        promiseQ.then((dataQ) => {
+
+            const promiseT = insertT(tags, dataQ[0].id);
+            promiseT.then((dataT) => {
+                knex.select()
+                    .from('questions')
+                    .where({is_answered: false})
+                    .orderBy('when_asked')
+                    .then((questions) => {
+                        resolve({
+                            questions: questions,
+                            currentQuestion: {
+                                questionID: dataQ[0].id,
+                                questionText: dataQ[0].question_text,
+                                whenAsked: dataQ[0].when_asked,
+                                messages: [],
+                                tags: dataT[0]
+                            }
+                        });
+                    })
+                    .catch((err) => {
+                        reject(err);
+                    });
+            })
+            .catch((err) => {
+                reject(err);
             });
-        })
-        .catch((err) => {
-            reject(err);
         });
+
+        // TODO: Write if statement here for if tags were passed in, do insert to questions_tags and update select to be a join query returning tags also for each question and map this in component.
+
     });
 };
 
 let insertQ = (questionText, userID) => {
-    console.log(questionText, userID);
-    knex.insert({question_text: questionText, user_id: userID, is_answered: false })
-    .into('questions')
-    .returning(['id', 'question_text', 'when_asked'])
-    .then((data) => {
-        return data[0];
-    })
-    .catch((err) => {
-        console.error(err);
+    return new Promise((resolve, reject) => {
+        knex.insert({question_text: questionText, user_id: userID, is_answered: false})
+            .into('questions')
+            .returning(['id', 'question_text', 'when_asked'])
+            .then((data) => {
+                resolve(data);
+            })
+            .catch((err) => {
+                reject(err);
+            });
     });
-}
+};
 
 let insertT = (tags, questionID) => {
-    console.log(tags, questionID);
-    let addedTags = []
-    tags.forEach((tag) => {
-        knex.insert({ tag: tag, question_id: questionID})
-        .into('tags')
-        .returning('tag')
-        .then(() => {
-            console.log('post tag success', tag);
-            addedTags.push(tag)
-        })
-        .catch((err) => {
-            console.log('post tag error', err, tag);
+    return new Promise((resolve, reject) => {
+        let addedTags = [];
+        let completed = 0;
+        tags.forEach((tag) => {
+            knex.insert({
+                tag: tag,
+                question_id: questionID
+            })
+                .into('tags')
+                .returning('tag')
+                .then(() => {
+                    console.log('post tag success', tag);
+                    addedTags.push(tag)
+                })
+                .catch((err) => {
+                    console.log('post tag error', err, tag);
+                });
+            completed++;
+            if (completed === tags.length) {
+                resolve(addedTags);
+            }
         });
     });
-    return addedTags
-}
+};
 
 module.exports = postQuestion;
